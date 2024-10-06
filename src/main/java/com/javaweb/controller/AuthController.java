38a9b3f1d76440046ebe3264792140d78d15ee0c
MK <qscvdefb@gmail.com>
2024-10-06 16:29:45 +0700

    :v

--- src/main/java/com/javaweb/controller/AuthController.java
+++ src/main/java/com/javaweb/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.javaweb.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,16 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    private final Environment environment;
+
+    public AuthController(Environment environment) {
+        this.environment = environment;
+    }
+//    @Value("${PORT}")
+//    private final String PORT;
+
     @GetMapping("/refreshToken")
     public String refreshToken() {
-
-        System.out.println(System.getenv("PORT"));
-        return System.getenv("PORT");
+        return "test";
     }
 
     @GetMapping("/login")
     public String login() {
-        return "login";
+//        Dotenv dotenv = Dotenv.load();
+        String port = environment.getProperty("port");
+        String dbUrl = "hi";
+
+        System.out.println(port);
+        return port;
     }
 
     @GetMapping("/res")
