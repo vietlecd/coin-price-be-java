d1def5a33b04d06ae85f9a7a0d923ea81d262a9f
MK <qscvdefb@gmail.com>
2024-10-10 20:43:27 +0700

    Update InterceptorConfig.java

--- src/main/java/com/javaweb/config/InterceptorConfig.java
+++ src/main/java/com/javaweb/config/InterceptorConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class InterceptorConfig implements WebMvcConfigurer {
     @Autowired
     private AuthenInterceptor authInterceptor;
 
