4ee91c1a0614da597f525accffebfe547fc82449
MK <qscvdefb@gmail.com>
2024-10-15 17:37:26 +0700

    ;:) đúng ra phải ignore cái này mà quên

--- src/main/java/com/javaweb/controller/TestController.java
+++ src/main/java/com/javaweb/controller/TestController.java
@@ -21,35 +21,5 @@ public class TestController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/findId")
-    public Optional<userData> findId(@RequestBody userData user){
-        System.out.println(user);
-
-        return userRepository.findById(user.getId());
-    }
-
-    @GetMapping("/findUser")
-    public userData findAll(@RequestBody userData user){
-        System.out.println(user);
-
-        return userRepository.findByUsername(user.getUsername());
-    }
-
-    @GetMapping("/getKey")
-    public String getKey() {
-        try {
-            return CreateToken.createToken("hehe", "123");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @GetMapping("/getDecode")
-    public LoginRequest getDecode() {
-        try {
-            return CreateToken.decodeToken("eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtZW1heSIsInN1YiI6ImhlaGUiLCJwYXNzd29yZCI6IjEyMyIsImV4cCI6MTcyODMyMTUxNX0.lp7ng7rTYZSi1TjZ6xNx4OwGYVcRHzuQinNMRXLwWvQ");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+    
 }
