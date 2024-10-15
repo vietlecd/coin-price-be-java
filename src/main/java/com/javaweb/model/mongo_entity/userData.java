3cba461c9fdcac111ecbbdba77ce0e4882bed40c
MK <qscvdefb@gmail.com>
2024-10-15 17:28:18 +0700

    Update userData.java

--- src/main/java/com/javaweb/model/mongo_entity/userData.java
+++ src/main/java/com/javaweb/model/mongo_entity/userData.java
@@ -2,18 +2,23 @@ package com.javaweb.model.mongo_entity;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "userData")
 public class userData {
-    @Id
-    private String id;
-
+    @Indexed(unique = true)
     private String username;
+
     private String name;
     private String password;
-    private String token;
+    private String email;
+    private Integer vip_role;
+    private List<String> ip_list;
 }
