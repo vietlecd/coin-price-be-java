0fc6ce52741880cbe2e365b2087b4766eb2b7934
luatto0303 <luat.to0303@hcmut.edu.vn>
2024-10-23 16:47:27 +0700

    fix socket and notification

--- src/main/java/com/javaweb/controller/BinanceController.java
+++ src/main/java/com/javaweb/controller/BinanceController.java
@@ -1,30 +1,13 @@
 package com.javaweb.controller;
 
-import com.javaweb.connect.impl.ListingWebSocketService;
-import com.javaweb.service.trigger.BinanceTokenListingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URI;
 
 @RestController
 public class BinanceController {
 
-    private final BinanceTokenListingService tokenService;
-
-    public BinanceController(BinanceTokenListingService tokenService) {
-        this.tokenService = tokenService;
-    }
-
     @GetMapping("/start")
-    public String startWebSocketClient() {
-        try {
-            URI uri = new URI("wss://stream.binance.com:9443/ws"); // Địa chỉ WebSocket của Binance
-            new ListingWebSocketService(uri, tokenService);
-            return "WebSocket connection started.";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Failed to start WebSocket connection: " + e.getMessage();
-        }
+    public String start() {
+        return "WebSocket connection has already started with server startup.";
     }
 }
