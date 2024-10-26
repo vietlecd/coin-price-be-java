993cdd6ff6f463ba1a7cd694383bec4408df8240
vietlecd <120305631+vietlecd@users.noreply.github.com>
2024-10-26 10:28:21 +0700

    1

--- src/main/java/com/javaweb/connect/impl/KlineWebSocketService.java
+++ src/main/java/com/javaweb/connect/impl/KlineWebSocketService.java
@@ -6,6 +6,7 @@ import com.javaweb.config.WebSocketConfig;
 import com.javaweb.connect.IConnectToWebSocketService;
 import com.javaweb.service.impl.KlineDataService;
 import com.javaweb.service.impl.SpotPriceDataService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
@@ -14,34 +15,36 @@ import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class KlineWebSocketService extends TextWebSocketHandler{
-    @Autowired
+    private final Set<String> subscribedSymbols = ConcurrentHashMap.newKeySet();
     private KlineDataService klineDataService;
-
-    @Autowired
     private final ObjectMapper objectMapper = new ObjectMapper();
-
-    @Autowired
     private WebSocketConfig webSocketConfig;
-
-    @Autowired
     private WebSocketClient webSocketClient;
 
 
 
-    private String buildKlineWebSocketUrl(List<String> streams) {
+    private String buildKlineWebSocketUrl(Set<String> streams) {
         String streamParam = streams.stream()
                 .map(s -> s.toLowerCase() + "@kline_1s")
                 .collect(Collectors.joining("/"));
         return "wss://stream.binance.com/stream?streams=" + streamParam;
     }
 
-    public void connectToWebSocket(List<String> streams) {
-        String wsUrl = buildKlineWebSocketUrl(streams);
-        webSocketConfig.connectToWebSocket(wsUrl, webSocketClient, this);
+    public synchronized void connectToWebSocket(List<String> streams) {
+        boolean hasNewSymbols = subscribedSymbols.addAll(streams);
+
+        if (hasNewSymbols) {
+            String wsUrl = buildKlineWebSocketUrl(subscribedSymbols);
+            webSocketConfig.connectToWebSocket(wsUrl, webSocketClient, this);
+        }
+
     }
 
     @Override
--- src/main/java/com/javaweb/connect/impl/SpotWebSocketService.java
+++ src/main/java/com/javaweb/connect/impl/SpotWebSocketService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javaweb.config.WebSocketConfig;
 import com.javaweb.connect.IConnectToWebSocketService;
 import com.javaweb.service.impl.SpotPriceDataService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
@@ -13,32 +14,33 @@ import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class SpotWebSocketService extends TextWebSocketHandler implements IConnectToWebSocketService {
-
-    @Autowired
+    private final Set<String> subscribedSymbols = ConcurrentHashMap.newKeySet();
     private SpotPriceDataService spotPriceDataService;
-
-    @Autowired
     private final ObjectMapper objectMapper = new ObjectMapper();
-
-    @Autowired
     private WebSocketConfig webSocketConfig;
-
-    @Autowired
     private WebSocketClient webSocketClient;
 
-    private String buildSpotWebSocketUrl(List<String> streams) {
+    private String buildSpotWebSocketUrl(Set<String> streams) {
         String streamParam = streams.stream().map(s -> s.toLowerCase() + "@ticker").collect(Collectors.joining("/"));
         return "wss://stream.binance.com:9443/stream?streams=" + streamParam;
     }
 
-    public void connectToWebSocket(List<String> streams, boolean isTriggerRequest){
-        String wsUrl = buildSpotWebSocketUrl(streams);
-        // Truyền cờ isTriggerRequest trực tiếp vào handler
-        webSocketConfig.connectToWebSocket(wsUrl, webSocketClient, new SpotWebSocketHandler(isTriggerRequest));
+    public synchronized void connectToWebSocket(List<String> streams, boolean isTriggerRequest){
+
+        boolean hasNewSymbols = subscribedSymbols.addAll(streams);
+
+        if (hasNewSymbols) {
+            String wsUrl = buildSpotWebSocketUrl(subscribedSymbols);
+            // Truyền cờ isTriggerRequest trực tiếp vào handler
+            webSocketConfig.connectToWebSocket(wsUrl, webSocketClient, new SpotWebSocketHandler(isTriggerRequest));
+        }
     }
 
     private class SpotWebSocketHandler extends TextWebSocketHandler {
