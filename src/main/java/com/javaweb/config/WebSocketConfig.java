aba716304e8afd2de3663ef3bf6882f9aa565837
samgiahuyfsdev <126471803+samgiahuyfsdev@users.noreply.github.com>
2024-10-27 21:18:07 +0700

    xoa config

--- src/main/java/com/javaweb/config/WebSocketConfig.java
+++ src/main/java/com/javaweb/config/WebSocketConfig.java
@@ -1,58 +1,58 @@
-package com.javaweb.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.client.WebSocketClient;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-import javax.annotation.PreDestroy;
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-
-import static org.apache.tomcat.jni.Socket.close;
-import static org.apache.tomcat.jni.Socket.shutdown;
-
-@Configuration
-public class WebSocketConfig {
-
-    private WebSocketSession webSocketSession;
-
-    @Bean
-    public WebSocketClient webSocketClient() {
-        return new StandardWebSocketClient();
-    }
-
-    public void connectToWebSocket(String webSocketUrl, WebSocketClient webSocketClient, TextWebSocketHandler handler) {
-        //Việt: thêm tính năng check HTTPS
-        if (webSocketUrl.startsWith("https://")) {
-            webSocketUrl = webSocketUrl.replace("https://", "wss://");
-        } else if (webSocketUrl.startsWith("http://")) {
-            webSocketUrl = webSocketUrl.replace("http://", "ws://");
-        }
-
-        try {
-            this.webSocketSession = webSocketClient.doHandshake(handler, webSocketUrl).get();
-            System.out.println("Connected to WebSocket at: " + webSocketUrl);
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //Cập nhat phan tắt web socket tránh sự cố
-    public void closeWebSocketSession() {
-        if (this.webSocketSession != null && this.webSocketSession.isOpen()) {
-            try {
-                this.webSocketSession.close();
-                System.out.println("WebSocket session closed.");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    @PreDestroy
-    public void onDestroy() {
-        closeWebSocketSession();
-    }
-}
+//package com.javaweb.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.WebSocketSession;
+//import org.springframework.web.socket.client.WebSocketClient;
+//import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+//import org.springframework.web.socket.handler.TextWebSocketHandler;
+//
+//import javax.annotation.PreDestroy;
+//import java.io.IOException;
+//import java.util.concurrent.ExecutionException;
+//
+//import static org.apache.tomcat.jni.Socket.close;
+//import static org.apache.tomcat.jni.Socket.shutdown;
+//
+//@Configuration
+//public class WebSocketConfig {
+//
+//    private WebSocketSession webSocketSession;
+//
+//    @Bean
+//    public WebSocketClient webSocketClient() {
+//        return new StandardWebSocketClient();
+//    }
+//
+//    public void connectToWebSocket(String webSocketUrl, WebSocketClient webSocketClient, TextWebSocketHandler handler) {
+//        //Việt: thêm tính năng check HTTPS
+//        if (webSocketUrl.startsWith("https://")) {
+//            webSocketUrl = webSocketUrl.replace("https://", "wss://");
+//        } else if (webSocketUrl.startsWith("http://")) {
+//            webSocketUrl = webSocketUrl.replace("http://", "ws://");
+//        }
+//
+//        try {
+//            this.webSocketSession = webSocketClient.doHandshake(handler, webSocketUrl).get();
+//            System.out.println("Connected to WebSocket at: " + webSocketUrl);
+//        } catch (InterruptedException | ExecutionException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    //Cập nhat phan tắt web socket tránh sự cố
+//    public void closeWebSocketSession() {
+//        if (this.webSocketSession != null && this.webSocketSession.isOpen()) {
+//            try {
+//                this.webSocketSession.close();
+//                System.out.println("WebSocket session closed.");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//    @PreDestroy
+//    public void onDestroy() {
+//        closeWebSocketSession();
+//    }
+//}
