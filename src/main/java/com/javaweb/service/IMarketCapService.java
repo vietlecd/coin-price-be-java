3d9ce54fecba3a3fa57cfd7e4aa2bc61c4e81350
samgiahuyfsdev <126471803+samgiahuyfsdev@users.noreply.github.com>
2024-10-27 23:03:16 +0700

    xoa tat ca tru service webhook

--- src/main/java/com/javaweb/service/IMarketCapService.java
+++ src/main/java/com/javaweb/service/IMarketCapService.java
@@ -1,9 +1,9 @@
-package com.javaweb.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface IMarketCapService {
-
-    List<Map<String, Object>> GetMarketData(List<String> symbols);
-}
+//package com.javaweb.service;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//public interface IMarketCapService {
+//
+//    List<Map<String, Object>> GetMarketData(List<String> symbols);
+//}
