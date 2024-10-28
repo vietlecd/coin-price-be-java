dad3b8c4ae83f7b51f25f61ecb2b1ac89a962a48
MK <qscvdefb@gmail.com>
2024-10-28 08:54:06 +0700

    sủa cookie lần 2

--- src/main/java/com/javaweb/service/authServices/LoginFunc.java
+++ src/main/java/com/javaweb/service/authServices/LoginFunc.java
@@ -2,8 +2,9 @@ package com.javaweb.service.authServices;
 
 import com.javaweb.model.mongo_entity.userData;
 import com.javaweb.service.CreateToken;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,12 +25,19 @@ public class LoginFunc {
     public static void setCookie(String username, HttpServletResponse res) throws Exception {
         String token = CreateToken.createToken(username);
 
-        Cookie cookie = new Cookie("token", token);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(false);
-        cookie.setPath("/");
-        cookie.setMaxAge(60 * 60 * 13);
+        ResponseCookie cookie = ResponseCookie.from("token", token)
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(60*60*13)
+                .build();
 
-        res.setHeader("Set-Cookie", cookie + "; SameSite=None");
+        String cookieHeader = cookie.toString() + "; SameSite=None";
+//        Cookie cookie = new Cookie("token", token);
+//        cookie.setHttpOnly(true);
+//        cookie.setSecure(false);
+//        cookie.setPath("/");
+//        cookie.setMaxAge(60 * 60 * 13);
+
+        res.setHeader(HttpHeaders.SET_COOKIE, cookieHeader);
     }
 }
